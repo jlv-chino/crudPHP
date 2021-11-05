de1bd585ad6e8676e8c924dba233c4782e66d0c2
José Luis Vilches <jose.luis.comvilches@gmail.com>
2021-11-05 02:05:18 -0300

    Finalizamos la validación del lado cliente (Registro de usuario)

--- views/js/validarRegistro.js
+++ views/js/validarRegistro.js
@@ -4,9 +4,11 @@ function validarRegistro(){
     let usuario = document.querySelector("#usuarioRegistro").value;
     let password = document.querySelector("#passwordRegistro").value;
     let email = document.querySelector("#emailRegistro").value;
+    let terminos = document.querySelector("#terminos").checked;
 
-    let expresionRegular = /^[a-zA-Z0-9]*$/;  
-    
+    let expresionRegular = /^[a-zA-Z0-9]*$/; 
+    let expresionEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+  
     //VALIDAR USUARIO
     if(usuario != "" && usuario.length > 6){
 
@@ -21,6 +23,41 @@ function validarRegistro(){
         alert("El campo nombre contiene caracteres especiales");
         
         return false;      
+
+    }
+
+    //VALIDAR PASSWORD
+    if(password != "" && password.length < 5){
+
+        alert("Escriba un mínimo de 6 caracteres en el campo password");
+        
+        return false;
+
+    }
+    
+    if(!expresionRegular.test(password)){
+
+        alert("El campo password contiene caracteres especiales");
+        
+        return false;      
+
+    }
+
+    //VALIDAR EMAIL
+    if(!expresionEmail.test(email)){
+
+        alert("Escriba su email correctamente");
+        
+        return false;      
+
+    }
+
+    //VALIDAR TERMINOS
+    if(!terminos){
+
+        alert("Acepte términos y condiciones para finalizar el registro");
+
+        return false;  
 
     }
 
--- views/modules/registro.php
+++ views/modules/registro.php
@@ -6,10 +6,12 @@
 	<input type="text" placeholder="Máximo 6 caracteres" name="usuarioRegistro" id="usuarioRegistro" maxlength="6" required>
 
 	<label for="passwordRegistro">Password: </label>
-	<input type="password" placeholder="Mínimo de 6 caracteres, incluir número/s y una mayúscula" name="passwordRegistro" id="passwordRegistro" minlength="6" required>
+	<input type="password" placeholder="Mínimo de 6 caracteres, incluir número/s y una mayúscula" name="passwordRegistro" id="passwordRegistro" minlength="6" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}" required>
 
 	<label for="emailRegistro">Email: </label>
 	<input type="email" placeholder="Escriba su email" name="emailRegistro" id="emailRegistro" required>
+
+	<p><input type="checkbox" id="terminos"><a href="#">Acepto términos y condiciones</a></p>
 
 	<input type="submit" value="Enviar">
 
